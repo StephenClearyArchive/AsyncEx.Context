6b64a61b7ba4eaddd50f8f5c38df6aa21c2faee2
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-09-17 21:34:31 -0400

    Move SynchronizationContextSwitcher to AsyncEx.Tasks.

--- src/Nito.AsyncEx.Context/SynchronizationContextSwitcher.cs
+++ src/Nito.AsyncEx.Context/SynchronizationContextSwitcher.cs
@@ -1,35 +1,3 @@
-﻿using System;
-using System.Threading;
+﻿using System.Runtime.CompilerServices;
 
-namespace Nito.AsyncEx
-{
-    /// <summary>
-    /// Utility class for temporarily switching <see cref="SynchronizationContext"/> implementations.
-    /// </summary>
-    public sealed class SynchronizationContextSwitcher : Disposables.SingleDisposable<object>
-    {
-        /// <summary>
-        /// The previous <see cref="SynchronizationContext"/>.
-        /// </summary>
-        private readonly SynchronizationContext _oldContext;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SynchronizationContextSwitcher"/> class, installing the new <see cref="SynchronizationContext"/>.
-        /// </summary>
-        /// <param name="newContext">The new <see cref="SynchronizationContext"/>.</param>
-        public SynchronizationContextSwitcher(SynchronizationContext newContext)
-            : base(new object())
-        {
-            _oldContext = SynchronizationContext.Current;
-            SynchronizationContext.SetSynchronizationContext(newContext);
-        }
-
-        /// <summary>
-        /// Restores the old <see cref="SynchronizationContext"/>.
-        /// </summary>
-        protected override void Dispose(object context)
-        {
-            SynchronizationContext.SetSynchronizationContext(_oldContext);
-        }
-    }
-}
+[assembly: TypeForwardedTo(typeof(Nito.AsyncEx.SynchronizationContextSwitcher))]
